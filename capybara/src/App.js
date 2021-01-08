9e6e80c254f136c7967f91d4aed6ef87a20f6a44
suwoons <sarahlim2612@gmail.com>
2021-01-08 15:02:15 +0800

    Add jump

--- capybara/src/App.js
+++ capybara/src/App.js
@@ -1,9 +1,59 @@
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 
 const App = () => {
+  const [jumping, toggleJump] = useState(false);
+  const [alive, toggleLife] = useState(true);
+  const playerRef = useRef();
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  });
+
+  useEffect(() => {
+    if (jumping) {
+      const timer = setTimeout(() => {
+        toggleJump(false);
+      }, 500);
+      return () => clearTimeout(timer);
+    }
+  }, [jumping]);
+
+  const handleKeyDown = (e) => {
+    const { key } = e;
+    // jump when SPACEBAR is pressed
+    if (key === " ") {
+      playerJump();
+    }
+  };
+
+  const playerJump = () => {
+    toggleJump(true);
+  };
+
+  const checkAlive = () => {
+    // console.log(parseInt(window.getComputedStyle(playerRef).getPropertyValue("top")));
+    console.log(playerRef.offsetTop);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      checkAlive();
+    }, 100);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="game">
-      <div id="player"></div>
+      <div id="player"
+        className={
+          jumping ? "jump" : ""
+        }
+        ref={playerRef}></div>
       <div id="cactus"></div>
     </div>
   );
